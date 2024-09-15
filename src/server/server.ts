60108cf3760e5c4ee7601f75655c2dd021552d82
pream <preamza02@gmail.com>
2024-09-15 16:23:01 +0700

    finsh server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,5 @@
-import path from "path"
 import * as gprc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
-import { ConnectDB } from "../configs/database";
 
 const port = 3000;
 const uri = `localhost:${port}`;
@@ -9,18 +7,19 @@ const PROTO_FILE = "../../proto/package.proto";
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
 const grpcObject = gprc.loadPackageDefinition(packageDef);
 const packageService = grpcObject.packageService;
-
 const server = new gprc.Server();
 // Connect to Database
 ConnectDB();
+listServices.forEach(service => {
+    server.addService(service.service, service.implementation);
+});
 
-server.addService(packageService.PackageService.service, {
-    GetPackage: (call:any, callback:any) => {
-        const package = {
-        id: 1,
-        name: "Package 1",
-        description: "This is package 1"
-        };
-        callback(null, package);
+server.bindAsync(url, gprc.ServerCredentials.createInsecure(), (err, port) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-    });
+    console.log(`Server running at http://${url}`);
+    server.start();
+}
+);
