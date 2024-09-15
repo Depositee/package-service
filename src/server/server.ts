c08e92618dc02e39911fbe1e3c59b4a4c0fa28d7
pream <preamza02@gmail.com>
2024-09-15 15:36:14 +0700

    edit package.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,22 @@
+import path from "path"
 import * as gprc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 
 const port = 3000;
 const uri = `localhost:${port}`;
-const ProtoFile = "../../"
+const PROTO_FILE = "../../proto/package.proto";
+const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
+const grpcObject = gprc.loadPackageDefinition(packageDef);
+const packageService = grpcObject.packageService;
 const server = new gprc.Server();
 
-server.addService
+server.addService(packageService.PackageService.service, {
+    GetPackage: (call:any, callback:any) => {
+        const package = {
+        id: 1,
+        name: "Package 1",
+        description: "This is package 1"
+        };
+        callback(null, package);
+    }
+    });
